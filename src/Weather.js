73266e210474ecd8fe7655ca24a382957dc4f25f
MbaliPuso <123210276+MbaliPuso@users.noreply.github.com>
2023-07-06 10:49:25 +0200

    Updated my date property

--- src/Weather.js
+++ src/Weather.js
@@ -15,7 +15,7 @@ export default function Weather(props) {
       temperature: response.data.main.temp,
       wind: response.data.wind.speed,
       humidity: response.data.main.humidity,
-      date: new Date(response.data.time * 1000),
+      date: new Date(response.data.dt * 1000),
       icon: `http://openweathermap.org/img/wn/${
         response.data.weather[0].icon
       }@2x.png`,
